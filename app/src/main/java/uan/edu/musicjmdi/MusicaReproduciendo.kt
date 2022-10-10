4c168824755cf2ea1c962aa8232078b1d3d3eb48
Vento22L <dionisiorockkivn@gmail.com>
2022-10-09 23:26:00 -0500

    Cambios confirmados

--- app/src/main/java/uan/edu/musicjmdi/MusicaReproduciendo.kt
+++ app/src/main/java/uan/edu/musicjmdi/MusicaReproduciendo.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MusicaReproduciendo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reproduciendo_musica)
+        setContentView(R.layout.activity_musica_reproduciendo)
     }
 }
