5754e63a9508ec67a0423c09496f302d759943f5
Vento22L <dionisiorockkivn@gmail.com>
2022-10-27 10:07:53 -0500

    Cambios en MainActivity.kt y activity_main.xml

--- app/src/main/java/uan/edu/musicjmdi/MainActivity.kt
+++ app/src/main/java/uan/edu/musicjmdi/MainActivity.kt
@@ -3,6 +3,8 @@ package uan.edu.musicjmdi
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,10 +23,41 @@ class MainActivity : AppCompatActivity() {
         m.prepare()
         m
     }
+
+    val controllers by lazy{
+        listOf(R.id.prev, R.id.stop, R.id.play, R.id.next).map {
+            findViewById<MaterialButton>(it)
+        }
+    }
+
+    object ci{
+        val prev = 0
+        val stop = 1
+        val play = 2
+        val next = 3
+    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mp.start()
+        controllers[ci.play].setOnClickListener(this::playClicked)
+        controllers[ci.play].setOnClickListener(this::stopClicked)
+
+    }
+
+    fun playClicked(v: View){
+        if(!mp.isPlaying){
+            mp.start()
+        }
+        else{
+            mp.pause()
+        }
+    }
+
+    fun stopClicked(v: View){
+        if(mp.isPlaying){
+            mp.pause()
+        }
+        mp.seekTo(0)
     }
 }
