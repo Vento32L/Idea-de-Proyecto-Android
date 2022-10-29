1652feec5bbc013adba0cffde9fd18a3088bba42
Vento22L <dionisiorockkivn@gmail.com>
2022-10-29 17:38:07 -0500

    Cambios en AdaptadorCanciones.kt

--- app/src/main/java/uan/edu/musicjmdi/AdaptadorCanciones.kt
+++ app/src/main/java/uan/edu/musicjmdi/AdaptadorCanciones.kt
@@ -1,11 +1,23 @@
 package uan.edu.musicjmdi
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import uan.edu.musicjmdi.databinding.RowSongBinding
 
-class AdaptadorCanciones(val elementos:List<String>, val con: MainActivity){
+class AdaptadorCanciones (val elementos:List<String>, val con: MainActivity):
     RecyclerView.Adapter<AdaptadorCanciones.ViewHolder>()
 
-    class ViewHolder
+    class ViewHolder(val bind: RowSongBinding)
+        :RecyclerView.ViewHolder(bind.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val v = RowSongBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun 
 
 
-}
+
+
